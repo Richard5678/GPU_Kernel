d3b76bbf3d641a8e41e2dc431b78be8762a16ae0
Richard5678 <frqrichard@gmail.com>
2025-02-16 22:24:27 -0500

    removed kernel implementation from benchmark script

--- matmul.cu
+++ matmul.cu
@@ -6,20 +6,7 @@
 #include <stdexcept>
 #include <cublas_v2.h>
 #include <random> 
-
-// implementation of naive kernel
-__global__ void matmul(float* A, float* B, float* C, int m, int n, int s) {
-    const int x = blockIdx.x * blockDim.x + threadIdx.x;
-    const int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (y < m && x < n) {
-        float temp = 0;
-        for (int i = 0; i < s; i++) {
-            temp += A[y * s + i] * B[x + i * n];
-        }
-        C[y * n + x] = temp;
-    }
-}
+#include "matmul_kernels.h"
 
 
 template <typename Func>
