f723dc7b61e4f45d2cbb3bafffe9301b24dbd679
Richard5678 <frqrichard@gmail.com>
2025-02-17 14:13:49 -0500

    implemented naive cuda kernel for both row and column major storage format

--- matmul_kernels.cu
+++ matmul_kernels.cu
@@ -1,15 +1,29 @@
 #include "matmul_kernels.h"
 
-// implementation of naive kernel
-__global__ void matmul(float* A, float* B, float* C, int m, int n, int s) {
-    const int x = blockIdx.x * blockDim.x + threadIdx.x;
-    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+// implementation of naive kernel with column major storage format
+__global__ void matmul_naive_column_major(float* A, float* B, float* C, int m, int n, int s) {
+    const unsigned int c = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int r = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (y < m && x < n) {
-        float temp = 0;
+    if (r < m && c < n) {
+        float temp = 0.0f;
         for (int i = 0; i < s; i++) {
-            temp += A[y * s + i] * B[x + i * n];
+            temp += A[r * s + i] * B[c + i * n];
         }
-        C[y * n + x] = temp;
+        C[r * n + c] = temp;
+    }
+}
+
+
+__global__ void matmul_naive_row_major(float* A, float* B, float* C, int m, int n, int s) {
+    const unsigned int r = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int c = blockIdx.y * blockDim.y + threadIdx.y; 
+
+    if (r < m && c < n) {
+        float temp = 0.0f;
+        for (int i = 0; i < s; i++) {
+            temp += A[r * s + i] * B[c + i * n];
+        }
+        C[r * n + c] = temp;
     }
 }
