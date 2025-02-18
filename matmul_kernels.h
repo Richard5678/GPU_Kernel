865a68f8b817f86c9b63c9d84831c48e49e2d201
Richard5678 <frqrichard@gmail.com>
2025-02-17 19:20:17 -0500

    removed previous implementation files

--- matmul_kernels.h
+++
@@ -1,7 +0,0 @@
-#include <cuda_runtime.h>
-
-// naive kernel with column major storage format
-__global__ void matmul_naive_column_major(float *A, float *B, float *C, int m, int n, int s);
-
-// naive kernel row major storage format
-__global__ void matmul_naive_row_major(float *A, float *B, float *C, int m, int n, int s);
