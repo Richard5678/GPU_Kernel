b3b796fb0fd2560b95d4c1e931423529cb8da7c8
Richard5678 <frqrichard@gmail.com>
2025-02-19 12:45:58 -0500

    added documentation for 1D block tiling kernel

--- matmul_kernels.cuh
+++ matmul_kernels.cuh
@@ -68,13 +68,13 @@ __global__ void matmul_smbc(float *A, float *B, float *C, int m, int n, int s)
         //      - row index is 'start_idx + threadIdx.y
         //      - col index is 'col'
         if (start_idx + threadIdx.y < s && col < n)
-            Bs[threadIdx.y * BLOCK_SIZE + threadIdx.x] = B[(start_idx + threadIdx.y) * n + col];
+            Bs[idx_in_block] = B[(start_idx + threadIdx.y) * n + col];
         else
             Bs[idx_in_block] = 0.0f;
 
         __syncthreads();
 
-        // Accumulate partial dot product for the current tile
+        // Accumulate partial dot product for the current block
         for (int i = 0; i < BLOCK_SIZE; i++)
         {
             temp += As[threadIdx.y * BLOCK_SIZE + i] * Bs[threadIdx.x + i * BLOCK_SIZE];
@@ -97,7 +97,7 @@ template <const int BM, const int BS, const int BN, const int TM>
 __global__ void matmul_1D_block_tiling(float *A, float *B, float *C, int m, int n, int s)
 {
     const int col = blockIdx.x * blockDim.x + threadIdx.x;
-    const int row = blockIdx.y * BM + threadIdx.y * TM;
+    const int row = blockIdx.y * BM + threadIdx.y * TM; // row of the first element of the vertical tile
 
     __shared__ float As[BM * BS];
     __shared__ float Bs[BS * BN];
@@ -106,7 +106,16 @@ __global__ void matmul_1D_block_tiling(float *A, float *B, float *C, int m, int 
 
     for (int start_idx = 0; start_idx < s; start_idx += BS)
     {
+        // 1D index of the thread in block As
         const int idx_in_block_a = threadIdx.x * BS + threadIdx.y;
+
+        // For matrix A:
+        //      - row index is 'blockIdx.y * BM + threadIdx.x'
+        //          - 'blockIdx.y * BM' -> row index of the frist row of the block
+        //          - 'threadIdx.x' -> row index of the thread in the block
+        //      - col index is 'start_idx + threadIdx.y'
+        //          - 'start_idx' -> col index of the first column in the block
+        //          - 'threadIdx.y' -> col index of the thread in the block
         if ((blockIdx.y * BM + threadIdx.x) < m && (start_idx + threadIdx.y) < s)
         {
             As[idx_in_block_a] = A[(blockIdx.y * BM + threadIdx.x) * s + (start_idx + threadIdx.y)];
@@ -116,7 +125,14 @@ __global__ void matmul_1D_block_tiling(float *A, float *B, float *C, int m, int 
             As[idx_in_block_a] = 0.0f;
         }
 
+        // 1D index of the thread in block Bs
         const int idx_in_block_b = threadIdx.y * BN + threadIdx.x;
+
+        // For matrix B:
+        //      - row index is 'start_idx + threadIdx.y'
+        //          - 'start_idx' -> row index of the frist row of the block
+        //          - 'threadIdx.y' -> row index of the thread in the block
+        //      - col index is 'col'
         if ((start_idx + threadIdx.y) < s && col < n)
         {
             Bs[idx_in_block_b] = B[(start_idx + threadIdx.y) * n + col];
@@ -128,6 +144,7 @@ __global__ void matmul_1D_block_tiling(float *A, float *B, float *C, int m, int 
 
         __syncthreads();
 
+        // accumulate partial dot product for the current 1D vertile block tile
         for (int i = 0; i < BS; i++)
         {
             const float B_val = Bs[i * BN + threadIdx.x];
@@ -140,10 +157,10 @@ __global__ void matmul_1D_block_tiling(float *A, float *B, float *C, int m, int 
         __syncthreads();
     }
 
-    // Add bounds checking when writing output
+    // update output of the 1D vertical block tile if within bound
     for (int j = 0; j < TM; j++)
     {
-        if (row + j < m && col < n) // Add bounds check here
+        if (row + j < m && col < n) // bounds check here
         {
             C[(row + j) * n + col] = output[j];
         }
